3bbe5b2a914831a88058e55cd6ebf3c2568b2741
klud <vincentgillibert@gmail.com>
2019-06-12 00:03:10 +0200

    Initi the app

---
+++ components/Navigation.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    fixed
+    app
+  >
+    <v-list>
+      <v-list-tile
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title" />
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <v-toolbar
+    :clipped-left="clipped"
+    fixed
+    app
+  >
+    <v-toolbar-side-icon @click="drawer = !drawer" />
+    <v-btn
+      icon
+      @click.stop="miniVariant = !miniVariant"
+    >
+      <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+    </v-btn>
+    <v-toolbar-title v-text="title" />
+    <v-spacer />
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Index',
+          to: '/'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Innso project'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app light>
+
+    <navigation />
+
+    <v-content>
+      <v-container>
+
+        <nuxt />
+
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navigation from '~/components/Navigation.vue';
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
