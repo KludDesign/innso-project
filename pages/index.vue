10f9ece8c064ddb3ebcd3c3a9766f85fb3a0f605
klud <vincentgillibert@gmail.com>
2019-06-12 21:39:57 +0200

    Add Status and Networks filtering

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,20 @@
     >
       <v-card>
         <v-card-title>
-          Nutrition
-          <v-spacer></v-spacer>
+          <v-select
+            :items="status"
+            label="Status"
+            v-model="statusType"
+          ></v-select>
+
+          <v-select
+            :items="contact_channel"
+            label="Réseau"
+            v-model="statusType"
+          ></v-select>
+        </v-card-title>
+
+        <v-card-title>
           <v-text-field
             v-model="search"
             append-icon="search"
@@ -19,11 +31,19 @@
             hide-details
           ></v-text-field>
         </v-card-title>
+
         <v-data-table
           :headers="headers"
-          :items="mockData"
+          :items="filteredItems"
           :search="search"
         >
+
+          <template slot="headerCell" slot-scope="props">
+            <span>
+              {{ props.header.text }}
+            </span>
+          </template>
+
           <template v-slot:items="props">
             <td>{{ props.item.customer.last_name }} {{ props.item.customer.first_name }}</td>
             <td class="text-xs-left">{{ props.item.customer.email }}</td>
@@ -32,11 +52,13 @@
             <td class="text-xs-left">{{ props.item.status }}</td>
             <td class="text-xs-left">{{ props.item.last_comment }}</td>
           </template>
+
           <template v-slot:no-results>
             <v-alert :value="true" color="error" icon="warning">
               Your search for "{{ search }}" found no results.
             </v-alert>
           </template>
+
         </v-data-table>
       </v-card>
     </v-flex>
@@ -44,11 +66,20 @@
 </template>
 
 <script>
-import Table from '~/components/Table.vue';
 import axios from 'axios';
+import draggable from 'vuedraggable'
 
 export default {
   components: {
+    draggable
+  },
+
+  computed: {
+    filteredItems() {
+      return this.mockData.filter((i) => {
+        return !this.statusType || i.status === this.statusType || i.contact_channel === this.statusType;
+      })
+    }
   },
 
   asyncData ({ params, error }) {
@@ -66,17 +97,42 @@ export default {
   data () {
     return {
       search: '',
+      statusType: null,
+      status: ['finished', 'inProgress', 'reserved', 'toTreat', 'waiting'],
+      contact_channel: ['facebook', 'file', 'form', 'messenger', 'twiter'],
       headers: [
         {
+          id: 0,
           text: 'Nom',
           align: 'left',
           value: 'customer.last_name'
         },
-        { text: 'Email', value: 'customer.email' },
-        { text: 'Téléphone', value: 'customer.phone' },
-        { text: 'Réseau', value: 'contact_channel' },
-        { text: 'Status', value: 'status' },
-        { text: 'Commentaire', value: 'last_comment', sortable: false }
+        {
+          id: 1,
+          text: 'Email',
+          value: 'customer.email'
+        },
+        {
+          id: 2,
+          text: 'Téléphone',
+          value: 'customer.phone'
+        },
+        {
+          id: 3,
+          text: 'Réseau',
+          value: 'contact_channel'
+        },
+        {
+          id: 4,
+          text: 'Status',
+          value: 'status'
+        },
+        {
+          id: 5,
+          text: 'Commentaire',
+          value: 'last_comment',
+          sortable: false
+        }
       ]
     }
   }
